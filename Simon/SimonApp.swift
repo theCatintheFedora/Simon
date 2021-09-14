e87f72ad67c92c82b04a98d31b30b0d4d5c60c2f
Timothy Price <Imnotsaying@gmail.com>
2021-09-14 10:43:42 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Student on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
